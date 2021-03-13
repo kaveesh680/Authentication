7b1392ae4738b4e3a35b21744f8553f29695c363
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-03-13 15:16:44 +0530

    Hashing passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-var encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
@@ -24,8 +24,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ["password"]} );
 
 const User = mongoose.model("User", userSchema);
 
@@ -49,7 +47,7 @@ app.route("/login")
       email: req.body.username,
     }, function(err, foundUser) {
       if (!err) {
-        if (foundUser.password === req.body.password) {
+        if (foundUser.password === md5(req.body.password)) {
           res.render("secrets");
         } else {
           console.log("Wrong password");
@@ -73,7 +71,7 @@ app.route("/register")
 
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
